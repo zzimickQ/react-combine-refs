9f90364cbcc0e198291cb42e46efe9b410974149
Zemichael Tesfaye <zzimick@gmail.com>
2020-08-20 12:28:44 +0300

    doc and type - added documentation and type defenition for end user.

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export default function useCombinedRefs(...refs: React.Ref): React.Ref;
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import { useRef, useEffect } from 'react'
 
-
+/**
+ * useCombinedRefs creates a ref from refs that listen
+ * to the value of the returned ref.
+ * 
+ * @param  {...React.Ref} refs references to be combined
+ * @returns {React.Ref} the reference that contains all the passed refs
+ */
 function useCombinedRefs(...refs) {
     const masterRef = useRef()
 
